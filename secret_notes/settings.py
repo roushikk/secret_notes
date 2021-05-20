dbcade272bbf0ee2c5f805d369f8cad36283a647
Roushik <roushikk@gmail.com>
2021-05-20 13:59:26 +0530

    Changed INSTALLED_APPS structure

--- secret_notes/settings.py
+++ secret_notes/settings.py
@@ -29,24 +29,17 @@ ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split()
 
 # Application definition
 
-DJANGO_APPS = [
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local
+    'notes',
 ]
-
-THIRD_PARTY_APPS = [
-
-]
-
-LOCAL_APPS = [
-    'notes'
-]
-
-INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
