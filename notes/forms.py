c5fe63ff9413759883b4e10c4d868de3b0ef8460
Roushik <roushikk@gmail.com>
2021-05-21 14:58:42 +0530

    Added logic and UI

---
+++ notes/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.utils.timezone import now
+from .models import Note
+
+class NoteCreateForm(forms.ModelForm):
+    password2 = forms.CharField(widget=forms.PasswordInput(), required=False)
+    expires_at = forms.DateTimeField(input_formats=['%d-%m-%Y %H:%M:%S'], required=False)
+    class Meta:
+        model = Note
+        fields = [
+            'title',
+            'content',
+            'password',
+            'allowed_reads',
+            'notify_email',
+            'display_confirmation',
+            'expires_at'
+        ]
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+
+    def clean_allowed_reads(self):
+        allowed_reads = self.cleaned_data['allowed_reads']
+        if allowed_reads < 1:
+            self.add_error('allowed_reads', 'Allowed reads cannot be less than 1')
+        return allowed_reads
+
+    def clean_expires_at(self):
+        expires_at = self.cleaned_data['expires_at']
+        if expires_at and expires_at < now():
+            self.add_error('expires_at', 'Expires at should be after current time')
+
+        return expires_at
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data['password']
+        password2 = cleaned_data['password2']
+
+        if password and password != password2:
+            self.add_error('password2', 'Password do not match')
+
+        return cleaned_data
--- notes/views.py
+++ notes/views.py
@@ -1,1 +1,68 @@
-# Create your views here.
+from django.shortcuts import reverse, get_object_or_404
+from django.views import generic
+
+from .forms import NoteCreateForm
+from .models import Note
+from django.http import Http404
+from django.utils.timezone import now
+
+class HomeView(generic.CreateView):
+    template_name = 'home.html'
+    form_class = NoteCreateForm
+
+    def get_success_url(self):
+        return f'{reverse("created")}?s={self.object.slug}'
+
+
+class CreatedView(generic.TemplateView):
+    template_name = 'created.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        slug = self.request.GET.get('s')
+        context['link'] = get_object_or_404(Note, slug=slug).get_absolute_url()
+        return context
+
+
+class NoteView(generic.DetailView):
+    template_name = 'note.html'
+    model = Note
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        if obj.allowed_reads <= obj.times_read:
+            raise Http404('Note not found.')
+
+        if obj.expires_at and obj.expires_at < now():
+            raise Http404('Note not found.')
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        if obj.password:
+            self.template_name = 'note_locked.html'
+        else:
+            obj.times_read += 1
+            obj.save()
+
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        password = request.POST.get('password')
+        self.object = self.get_object()
+
+        if password == self.object.password:
+            self.object.times_read += 1
+            self.object.save()
+        else:
+            self.template_name = 'note_locked.html'
+
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
+home = HomeView.as_view()
+created = CreatedView.as_view()
+note = NoteView.as_view()
